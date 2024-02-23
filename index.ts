a2b078635d0fef79ba6862ce737c9f7fe57eaf41
Laurin-Notemann <laurin.notemann@gmail.com>
2024-02-23 16:18:00 +0100

    make express app

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("<p>Moin meister</p>")
+})
+
+app.listen(5000, () => {
+  console.log("listening on port 5000");
+
+})
